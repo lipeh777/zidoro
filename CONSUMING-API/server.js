a2103b1fe2955075670fa1656b0596e5a327902e
lipeh777 <filipeizidorio@outlook.com>
2021-04-28 19:14:36 -0300

    first commit

---
+++ CONSUMING-API/server.js
@@ -0,0 +1,28 @@
+const cors = require('cors');
+const express = require('express');
+const axios = require('axios')
+const app = express();
+
+app.use(cors())
+
+app.get('/', async(req, res)=>{
+
+try{
+	const {data} = await axios('https://api.github.com/search/repositories?q=user%3Atakenet%20language%3Ac%23?per_page=100')
+	
+	const ber = []
+	data.items.forEach(({full_name, description, created_at})=>{
+		ber.push({"full_name": full_name, "description": description})
+		console.log(ber)
+		// console.log(full_name," --- ", description)
+	})
+	return res.json(ber.sort().slice(0,5))
+
+	// console.log(res.json(data.items.name))
+}catch(error){
+	console.error(error)
+}
+
+})
+
+app.listen('3600');
