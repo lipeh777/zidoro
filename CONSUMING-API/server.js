1d3c5cf792d66c76b7e56a09f04f6e715f1bb251
lipeh777 <filipeizidorio@outlook.com>
2021-04-28 23:10:38 -0300

    alterando o server.js

--- CONSUMING-API/server.js
+++ CONSUMING-API/server.js
@@ -8,19 +8,31 @@ app.use(cors())
 
 app.get('/cool', (req, res) => res.send(cool()))
 app.get('/', async(req, res)=>{
+		
 
-try{
-	const {data} = await axios('https://api.github.com/search/repositories?q=user%3Atakenet%20language%3Ac%23?per_page=100')
-	
-	const ber = []
-	data.items.forEach(({full_name, description, created_at})=>{
-		ber.push({"full_name": full_name, "description": description})
-		console.log(ber)
-		// console.log(full_name," --- ", description)
+	try{
+		const {data} = await axios('https://api.github.com/search/repositories?q=user%3Atakenet%20language%3Ac%23?per_page=100')
+
+		let ber = []
+		data.items.forEach(({full_name, description, created_at})=>{
+			ber.push({"full_name": full_name, "description": description, "created_at": Number(new Date(created_at).getTime())})
+			
 	})
-	return res.json(ber.sort().slice(0,5))
+		
+		ber = ber.sort(function(a, b){
+			if(a.created_at < b.created_at){
+				return -1
+			}if(b.created_at < a.created_at){
+				return 1
+			}
+			return 0
+		}).slice(0,5)
 
-	// console.log(res.json(data.items.name))
+		ber.forEach((data)=>{
+			delete data.created_at
+		})
+
+		return res.json(ber)
 }catch(error){
 	console.error(error)
 }
